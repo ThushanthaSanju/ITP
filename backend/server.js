8f72671b0000f1cc70199a0b940658f6c5e29b1b
Thushantha Sanju <thushanthasanju8@gmail.com>
2021-08-09 12:28:50 +0530

    document changes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
 
 const app = express();
 
-app.get('/api/products', (req,res)=>{
-    res.send(data.products);
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
-app.get('/',(req,res)=>{
-    res.send('Server is Ready');
-
+app.get("/", (req, res) => {
+  res.send("Server is Ready");
 });
 
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`Serve at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
 });
