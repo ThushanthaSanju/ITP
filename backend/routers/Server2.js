95bf2b14a70533254ab4995389270bcbe7434521
niwarthana98 <it20035358@my.sliit.lk>
2021-09-10 12:11:50 +0530

    Updated

--- backend/routers/Server2.js
+++ backend/routers/Server2.js
@@ -798,4 +798,570 @@ export default function OrderScreen(props) {
                         onSuccess={successPaymentHandler}
                       ></PayPalButton>
                     </>
+ import React, { useEffect, useState } from 'react';
+ import { useDispatch, useSelector } from 'react-redux';
+ import { Link } from 'react-router-dom';
+ import { register } from '../actions/userActions';
+ import LoadingBox from '../components/LoadingBox';
+ import MessageBox from '../components/MessageBox';
  
+ export default function RegisterScreen(props) {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [confirmPassword, setConfirmPassword] = useState('');
+ 
+   const redirect = props.location.search
+     ? props.location.search.split('=')[1]
+     : '/';
+ 
+   const userRegister = useSelector((state) => state.userRegister);
+   const { userInfo, loading, error } = userRegister;
+ 
+   const dispatch = useDispatch();
+   const submitHandler = (e) => {
+     e.preventDefault();
+     if(password !== confirmPassword){
+       alert('Password and Confirm password are not matching')
+     }else{
+       dispatch(register(name, email, password));
+     }
+     
+   };
+   useEffect(() => {
+     if (userInfo) {
+       props.history.push(redirect);
+     }
+   }, [props.history, redirect, userInfo]);
+   return (
+     <div>
+       <form className="form" onSubmit={submitHandler}>
+         <div>
+           <h1>Register</h1>
+         </div>
+         {loading && <LoadingBox></LoadingBox>}
+         {error && <MessageBox variant="danger">{error}</MessageBox>}
+         <div>
+           <label htmlFor="name">Name</label>
+           <input
+             type="text"
+             id="name"
+             placeholder="Enter name"
+             required
+             onChange={(e) => setName(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <label htmlFor="email">Email address</label>
+           <input
+             type="email"
+             id="email"
+             placeholder="Enter email"
+             required
+             onChange={(e) => setEmail(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <label htmlFor="password">Password</label>
+           <input
+             type="password"
+             id="password"
+             placeholder="Enter password"
+             required
+             onChange={(e) => setPassword(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <label htmlFor="confirmPassword">confirm Password</label>
+           <input
+             type="password"
+             id="confirmPassword"
+             placeholder="Enter confirm password"
+             required
+             onChange={(e) => setConfirmPassword(e.target.value)}
+           ></input>
+         </div>
+         <div>
+           <label />
+           <button className="primary" type="submit">
+             Register
+           </button>
+         </div>
+         <div>
+           <label />
+           <div>
+           Already have an account?<Link to={`/signin?redirect=${redirect}`}>Sign In</Link> //pass redirect to sign in
+           </div>
+         </div>
+       </form>
+     </div>
+   );
+ }
+ import React, { useEffect } from "react";
+ import { useDispatch, useSelector } from "react-redux";
+ import { Link, useParams } from 'react-router-dom';
+ import {
+   createProduct,
+   deleteProduct,
+   listProducts,
+ } from "../actions/productActions";
+ import LoadingBox from "../components/LoadingBox";
+ import MessageBox from "../components/MessageBox";
+ 
+ import {
+   PRODUCT_CREATE_RESET,
+   PRODUCT_DELETE_RESET,
+ } from "../constants/productConstants";
+ 
+ export default function ProductListScreen(props) {
+   const { pageNumber = 1 } = useParams();
+ 
+   const sellerMode = props.match.path.indexOf("/seller") >= 0;
+   const productList = useSelector((state) => state.productList);
+   const { loading, error, products, page, pages } = productList;
+   const productCreate = useSelector((state) => state.productCreate);
+   const {
+     loading: loadingCreate,
+     error: errorCreate,
+     success: successCreate,
+     product: createdProduct,
+   } = productCreate;
+ 
+   const productDelete = useSelector((state) => state.productDelete);
+   const {
+     loading: loadingDelete,
+     error: errorDelete,
+     success: successDelete,
+   } = productDelete;
+ 
+   const userSignin = useSelector((state) => state.userSignin);
+   const { userInfo } = userSignin;
+   const dispatch = useDispatch();
+   useEffect(() => {
+     if (successCreate) {
+       dispatch({ type: PRODUCT_CREATE_RESET });
+       props.history.push(`/product/${createdProduct._id}/edit`);
+     }
+     if (successDelete) {
+       dispatch({ type: PRODUCT_DELETE_RESET });
+     }
+     dispatch(
+       listProducts({ seller: sellerMode ? userInfo._id : '', pageNumber })
+     );
+   }, [
+     createdProduct,
+     dispatch,
+     props.history,
+     sellerMode,
+     successCreate,
+     successDelete,
+     userInfo._id,
+     pageNumber,
+   ]);
+ 
+   const deleteHandler = (product) => {
+     if (window.confirm("Are you sure to delete?")) {
+       dispatch(deleteProduct(product._id));
+     }
+   };
+   const createHandler = () => {
+     dispatch(createProduct());
+   };
+   return (
+     <div>
+       <div className="row">
+         <h1>Products</h1>
+         <button type="button" className="primary" onClick={createHandler}>
+           Create Product
+         </button>
+       </div>
+ 
+       {loadingDelete && <LoadingBox></LoadingBox>}
+       {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
+ 
+       {loadingCreate && <LoadingBox></LoadingBox>}
+       {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
+       {loading ? (
+         <LoadingBox></LoadingBox>
+       ) : error ? (
+         <MessageBox variant="danger">{error}</MessageBox>
+       ) : (
+         <>
+         <table className="table">
+           <thead>
+             <tr>
+               <th>ID</th>
+               <th>NAME</th>
+               <th>PRICE</th>
+               <th>CATEGORY</th>
+               <th>BRAND</th>
+               <th>ACTIONS</th>
+               </tr>
+               </thead>
+             <tbody>
+               {products.map((product) => (
+                 <tr key={product._id}>
+                   <td>{product._id}</td>
+                   <td>{product.name}</td>
+                   <td>{product.price}</td>
+                   <td>{product.category}</td>
+                   <td>{product.brand}</td>
+                   <td>
+                     <button
+                       type="button"
+                       className="small"
+                       onClick={() =>
+                         props.history.push(`/product/${product._id}/edit`)
+                       }
+                     >
+                       Edit
+                     </button>
+                     <button
+                       type="button"
+                       className="small"
+                       onClick={() => deleteHandler(product)}
+                     >
+                       Delete
+                     </button>
+                   </td>
+                 </tr>
+               ))}
+             </tbody>
+           </table>
+           <div className="row center pagination">
+             {[...Array(pages).keys()].map((x) => (
+               <Link
+                 className={x + 1 === page ? 'active' : ''}
+                 key={x + 1}
+                 to={`/productlist/pageNumber/${x + 1}`}
+               >
+                 {x + 1}
+               </Link>
+             ))}
+            </div>
+         </>
+       )}
+     </div>
+   );
+ }
+ 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { ORDER_CREATE_RESET } from '../constants/orderConstants';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+
+export default function PlaceOrderScreen(props) {
+  const cart = useSelector((state) => state.cart);
+  if (!cart.paymentMethod) {
+    props.history.push('/payment');
+  }
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { loading, success, error, order } = orderCreate;
+  const toPrice = (num) => Number(num.toFixed(2)); // 5.123 => "5.12" => 5.12
+  cart.itemsPrice = toPrice(
+    cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
+  );
+  cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
+  cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
+  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+  const dispatch = useDispatch();
+  const placeOrderHandler = () => {
+    dispatch(createOrder({ ...cart, orderItems: cart.cartItems }));
+  };
+  useEffect(() => {
+    if (success) {
+      props.history.push(`/order/${order._id}`);
+      dispatch({ type: ORDER_CREATE_RESET });
+    }
+  }, [dispatch, order, props.history, success]);
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
+      <div className="row top">
+        <div className="col-2">
+          <ul>
+            <li>
+              <div className="card card-body">
+                <h2>Shipping</h2>
+                <p>
+                  <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+                  <strong>Address: </strong> {cart.shippingAddress.address},
+                  {cart.shippingAddress.city}, {cart.shippingAddress.postalCode}
+                  ,{cart.shippingAddress.country}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Payment</h2>
+                <p>
+                  <strong>Method:</strong> {cart.paymentMethod}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Order Items</h2>
+                <ul>
+                  {cart.cartItems.map((item) => (
+                    <li key={item.product}>
+                      <div className="row">
+                        <div>
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="small"
+                          ></img>
+                        </div>
+                        <div className="min-30">
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </div>
+
+                        <div>
+                          {item.qty} x Rs.{item.price}.00 = Rs.{item.qty * item.price}.00
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>Order Summary</h2>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Items</div>
+                  <div>Rs.{cart.itemsPrice.toFixed(2)}.00</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Shipping</div>
+                  <div>Rs.{cart.shippingPrice.toFixed(2)}.00</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Tax</div>
+                  <div>Rs.{cart.taxPrice.toFixed(2)}.00</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>
+                    <strong> Order Total</strong>
+                  </div>
+                  <div>
+                    <strong>Rs.{cart.totalPrice.toFixed(2)}.00</strong>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={placeOrderHandler}
+                  className="primary block"
+                  disabled={cart.cartItems.length === 0}
+                >
+                  Place Order
+                </button>
+              </li>
+
+              {loading && <LoadingBox></LoadingBox>}
+              {error && <MessageBox variant="danger">{error}</MessageBox>}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { listProducts } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import Product from '../components/Product';
+import Rating from '../components/Rating';
+import { prices, ratings } from '../utils';
+
+export default function SearchScreen(props) {
+  const {
+    name = 'all',
+    category = 'all',
+    min = 0,
+    max = 0,
+    rating = 0,
+    order = 'newest',
+    pageNumber = 1,
+  } = useParams();
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
+  
+  const productCategoryList = useSelector((state) => state.productCategoryList);
+  const {
+    loading: loadingCategories,
+    error: errorCategories,
+    categories,
+  } = productCategoryList;
+  useEffect(() => {
+    dispatch(
+      listProducts({
+        pageNumber,
+        name: name !== 'all' ? name : '',
+        category: category !== 'all' ? category : '',
+        min,
+        max,
+        rating,
+        order,
+      })
+    );
+  }, [category, dispatch, max, min, name, order, rating, pageNumber]);
+
+  const getFilterUrl = (filter) => {
+    const filterPage = filter.page || pageNumber;
+    const filterCategory = filter.category || category;
+    const filterName = filter.name || name;
+    const filterRating = filter.rating || rating;
+    const sortOrder = filter.order || order;
+    const filterMin = filter.min ? filter.min : filter.min === 0 ? 0 : min;
+    const filterMax = filter.max ? filter.max : filter.max === 0 ? 0 : max;
+    return `/search/category/${filterCategory}/name/${filterName}/min/${filterMin}/max/${filterMax}/rating/${filterRating}/order/${sortOrder}/pageNumber/${filterPage}`;
+  };
+  return (
+    <div>
+      <div className="row">
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <div>{products.length} Results</div>
+        )}
+                <div>
+          Sort by{' '}
+          <select
+            value={order}
+            onChange={(e) => {
+              props.history.push(getFilterUrl({ order: e.target.value }));
+            }}
+          >
+            <option value="newest">Newest Arrivals</option>
+            <option value="lowest">Price: Low to High</option>
+            <option value="highest">Price: High to Low</option>
+            <option value="toprated">Avg. Customer Reviews</option>
+          </select>
+        </div>
+      </div>
+      <div className="row top">
+        <div className="col-1">
+          <h3>Department</h3>
+          <div>
+            {loadingCategories ? (
+              <LoadingBox></LoadingBox>
+            ) : errorCategories ? (
+              <MessageBox variant="danger">{errorCategories}</MessageBox>
+            ) : (
+              <ul>
+                <li>
+                  <Link
+                    className={'all' === category ? 'active' : ''}
+                    to={getFilterUrl({ category: 'all' })}
+                  >
+                    Any
+                  </Link>
+                </li>
+                {categories.map((c) => (
+                  <li key={c}>
+                    <Link
+                      className={c === category ? 'active' : ''}
+                      to={getFilterUrl({ category: c })}
+                    >
+                      {c}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <div>
+            <h3>Price</h3>
+            <ul>
+            {prices.map((p) => (
+                <li key={p.name}>
+                  <Link
+                     to={getFilterUrl({ min: p.min, max: p.max })}
+                     className={
+                       `${p.min}-${p.max}` === `${min}-${max}` ? 'active' : ''
+                     }
+                  >
+                    {p.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            </div>
+          <div>
+            <h3>Avg. Customer Review</h3>
+            <ul>
+              {ratings.map((r) => (
+                <li key={r.name}>
+                  <Link
+                    to={getFilterUrl({ rating: r.rating })}
+                    className={`${r.rating}` === `${rating}` ? 'active' : ''}
+                  >
+                    <Rating caption={' & up'} rating={r.rating}></Rating>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="col-3">
+          {loading ? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+          ) : (
+            <>
+              {products.length === 0 && (
+                <MessageBox>No Product Found</MessageBox>
+              )}
+              <div className="row center">
+                {products.map((product) => (
+                  <Product key={product._id} product={product}></Product>
+                ))}
+              </div>
+              <div className="row center pagination">
+                {[...Array(pages).keys()].map((x) => (
+                  <Link
+                    className={x + 1 === page ? 'active' : ''}
+                    key={x + 1}
+                    to={getFilterUrl({ page: x + 1 })}
+                  >
+                    {x + 1}
+                  </Link>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
