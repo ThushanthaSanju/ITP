d02c2526d5099565cc4c6a514572b64419a282e6
Tharushi1212 <it20000608@my.sliit.lk>
2021-10-06 22:11:55 +0530

    commenting and report editing

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef , useState} from "react";
 import { useReactToPrint } from "react-to-print";
 import { ComponentToPrint } from "../components/ComponentToPrint";
-
+import axios from 'axios';
 import { useDispatch, useSelector } from "react-redux";
 import { deleteUser, listUsers } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import jsPDF from 'jspdf';
 import { USER_DETAILS_RESET } from "../constants/userConstants";
 
 export default function UserListScreen(props) {
   //report
   const componentRef = useRef();
-  const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
-  });
+ 
+
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
@@ -26,21 +26,94 @@ export default function UserListScreen(props) {
   } = userDelete;
 
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(listUsers());
-    dispatch({
-      type: USER_DETAILS_RESET,
-    });
-  }, [dispatch, successDelete]);
+  // useEffect(() => {
+  //   dispatch(listUsers());
+  //   dispatch({
+  //     type: USER_DETAILS_RESET,
+  //   });
+  // }, [dispatch, successDelete]);
   const deleteHandler = (user) => {
     if (window.confirm("Are you sure?")) {
       dispatch(deleteUser(user._id));
     }
   };
+
+  const [usersProfile,setUserList] = useState([])
+   useEffect(() => {
+       axios.get("http://localhost:5000/userModel")
+       .then(res => setUserList(res.data))
+       .catch(error => console.log(error));
+   },[])
+
+  const [searchName,setNameSearch] = useState("");
+  function searchUsingName()
+  {
+    axios.get("http://localhost:5000/userModel/"+searchName)
+    .then(res => setUserList(res.data))
+    .catch(error => console.log(error));
+  }
+
+  
+  function cancelSearch()
+  {
+      setNameSearch("");
+      axios.get("http://localhost:5000/userModel")
+      .then(res => setUserList(res.data))
+      .catch(error => console.log(error));
+  }
+
+
+  console.log(usersProfile);
+  function handlePrint()
+  {
+    axios.get("http://localhost:5000/userModel")
+    .then(res => setUserList(res.data))
+    .catch(error => console.log(error));
+
+    var doc = new jsPDF('p', 'pt');
+    doc.setTextColor(254, 8, 8 );
+    doc.text(20, 20, "User List")
+    doc.text(25, 30, '---------------------------')
+    doc.addFont('helvetica', 'normal')
+    doc.setFontSize(12);
+    doc.setTextColor(3, 3, 3);
+    // eslint-disable-next-line no-lone-blocks
+    localStorage.setItem("usersProfile", JSON.stringify(usersProfile));
+    
+    // retrieving our data and converting it back into an array
+    var usersProfileData = localStorage.getItem("usersProfile");
+    var profile = JSON.parse(usersProfileData);
+
+    let count = 90;
+    doc.text(25, 50, "Total Users : "+profile.length)
+    doc.text(25, 80, "Data Structure : Name\t-\tEmail\t-\tIs Seller\t-\tIs Admin")
+    
+    doc.setFontSize(11);
+   for(var i = 0; i < profile.length; i++)
+   {
+       count = count+20;
+      doc.text(25, count, profile[i].name+" - "+profile[i].email+" - "+profile[i].isSeller+" - "+profile[i].isAdmin)
+   }
+   
+    doc.save('Users List.pdf')
+  }
   return (
     <div>
       <h1>Users</h1>
-      {loadingDelete && <LoadingBox></LoadingBox>}
+      <div class="mb-3 mt-5 row container">
+        <div class="col-10">
+            <div class="input-group mb-3">
+            &nbsp;&nbsp;<input type="text" class="form-control" placeholder="Type Name.."  aria-label="Recipient's username" aria-describedby="button-addon2" onChange={(e) =>{
+                setNameSearch(e.target.value);
+            }}/>
+            <button class="btn btn-dark" type="button" id="button-addon2" style={{backgroundColor:'black', color:'white'}} onClick={cancelSearch}>Cancel</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+        </div>
+        <div class="col-2">
+        <button class="btn btn-dark" type="button" id="button-addon2" style={{backgroundColor:'green', color:'white'}}  onClick={searchUsingName}>Search</button>
+        </div>
+     </div>
+      {/* {loadingDelete && <LoadingBox></LoadingBox>}
       {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
       {successDelete && (
         <MessageBox variant="success">User Deleted Successfully</MessageBox>
@@ -49,8 +122,9 @@ export default function UserListScreen(props) {
         <LoadingBox></LoadingBox>
       ) : error ? (
         <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-        <table className="table">
+      ) : ( */}
+       
+        <table className="table " style={{marginTop:'2%'}}>
           <thead>
             <tr>
               <th>ID</th>
@@ -62,7 +136,7 @@ export default function UserListScreen(props) {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+          {usersProfile.map((user,key) => (
               <tr key={user._id}>
                 <td>{user._id}</td>
                 <td>{user.name}</td>
@@ -89,7 +163,7 @@ export default function UserListScreen(props) {
             ))}
           </tbody>
         </table>
-      )}
+      {/* )} */}
       <div>
         <ComponentToPrint ref={componentRef} />
         <br />
